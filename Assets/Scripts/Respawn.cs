73adf456bb6c87d587d4d22b2d7f46d27d66afcb
Tox1cQueen <77941882+Tox1cQueen@users.noreply.github.com>
2023-11-07 19:30:33 +0000

    whitebox section 2

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class Respawn : MonoBehaviour
 {
 
-    public GameObject spawnPoint;
+   // public GameObject spawnPoint;
     public GameObject player;
+    public GameMaster GM;
+
+    void start()
+    {
+        GM = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+    }
 
     void OnTriggerEnter (Collider other)
     {
         if (other.transform.tag == "Player")
         {
-            player.transform.position = spawnPoint.transform.position;
+            player.transform.position = GM.lastCheckPointPos;
         }
     }
 }
