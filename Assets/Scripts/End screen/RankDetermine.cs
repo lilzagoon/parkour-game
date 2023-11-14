1c482276b05ea51c6ab5aa7299e3b9e9c0184cf1
Tox1cQueen <77941882+Tox1cQueen@users.noreply.github.com>
2023-11-14 04:51:24 +0000

    end screen rank fixes

--- Assets/Scripts/End screen/RankDetermine.cs
+++ Assets/Scripts/End screen/RankDetermine.cs
@@ -16,34 +16,35 @@ public class RankDetermine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(timer.roundedTime <= 55)
+        if (timer.currentTime >= 85)
         {
-            Debug.Log("S");
-            ES.rankPoints = 4500;
+            Debug.Log("D");
+            ES.rankPoints = 500;
         }
 
-        if (timer.roundedTime <= 65 && timer.roundedTime >= 55)
-        {
-            Debug.Log("A");
-            ES.rankPoints = 3500;
-        }
-
-        if (timer.roundedTime <= 75 && timer.roundedTime >= 65)
-        {
-            Debug.Log("B");
-            ES.rankPoints = 2500;
-        }
-
-        if (timer.roundedTime <= 85 && timer.roundedTime >= 75)
+        if (timer.currentTime >= 75 && timer.currentTime <= 85)
         {
             Debug.Log("C");
             ES.rankPoints = 1500;
         }
 
-        if (timer.roundedTime >= 85)
+        if (timer.currentTime >= 65 && timer.currentTime <= 75)
         {
-            Debug.Log("D");
-            ES.rankPoints = 500;
+            Debug.Log("B");
+            ES.rankPoints = 2500;
         }
+
+        if (timer.currentTime >= 55 && timer.currentTime <= 65)
+        {
+            Debug.Log("A");
+            ES.rankPoints = 3500;
+        }
+
+        if (timer.currentTime <= 55)
+        {
+            Debug.Log("S");
+            ES.rankPoints = 4500;
+        }
+
     }
 }
