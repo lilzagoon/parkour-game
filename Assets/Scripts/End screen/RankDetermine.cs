a18f5e8fc18607ef51c2cf5bbf3d843089e8316b
Tox1cQueen <77941882+Tox1cQueen@users.noreply.github.com>
2023-12-11 00:28:00 +0000

    GP level 1 and 2 made

--- Assets/Scripts/End screen/RankDetermine.cs
+++ Assets/Scripts/End screen/RankDetermine.cs
@@ -6,6 +6,11 @@ public class RankDetermine : MonoBehaviour
 {
     public Timer timer;
     public EndScreen ES;
+    public int Stime = 0;
+    public int Atime = 0;
+    public int Btime = 0;
+    public int Ctime = 0;
+    public int Dtime = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -16,31 +21,31 @@ public class RankDetermine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (timer.currentTime >= 85)
+        if (timer.currentTime >= Dtime)
         {
             Debug.Log("D");
             ES.rankPoints = 500;
         }
 
-        if (timer.currentTime >= 75 && timer.currentTime <= 85)
+        if (timer.currentTime >= Btime && timer.currentTime <= Ctime)
         {
             Debug.Log("C");
             ES.rankPoints = 1500;
         }
 
-        if (timer.currentTime >= 65 && timer.currentTime <= 75)
+        if (timer.currentTime >= Atime && timer.currentTime <= Btime)
         {
             Debug.Log("B");
             ES.rankPoints = 2500;
         }
 
-        if (timer.currentTime >= 55 && timer.currentTime <= 65)
+        if (timer.currentTime >= Stime && timer.currentTime <= Atime)
         {
             Debug.Log("A");
             ES.rankPoints = 3500;
         }
 
-        if (timer.currentTime <= 55)
+        if (timer.currentTime <= Stime)
         {
             Debug.Log("S");
             ES.rankPoints = 4500;
