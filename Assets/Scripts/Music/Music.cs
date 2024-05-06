c34626e1f0cc1771eafb78f002354db8b75c9576
SmolConnor <77941492+SmolConnor@users.noreply.github.com>
2024-05-06 17:06:10 +0100

    Cutscene and music

--- Assets/Scripts/Music/Music.cs
+++ Assets/Scripts/Music/Music.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Music : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private FMOD.Studio.EventInstance instance;
+
+    
+    public FMODUnity.EventReference fmodEvent;
+
+    public int sceneNum;
+   
     void Start()
     {
-        
+        sceneNum = SceneManager.GetActiveScene().buildIndex;
+        instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+        instance.start();
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        sceneNum = SceneManager.GetActiveScene().buildIndex;
+        instance.setParameterByName("Level", sceneNum);
+    }
+
+    public void Mute()
+    {
+        instance.setParameterByName("On or Off", 1);
+    }
+
+    public void UnMute()
+    {
+        instance.setParameterByName("On or Off", 0);
     }
 }
